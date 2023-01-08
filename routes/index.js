42c529914422fa9464eb9a64d6c4fc8da421d9e8
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2023-01-07 17:56:07 -0700

    uh

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
-const routes = require('express').Router();
+const routes = require("express").Router();
 
-routes.get('/', (req, res) => {
-  res.send('Sarah Birch');
+routes.get("/", (req, res) => {
+  res.send("Sarah Birch");
+});
+routes.get("/egg", (req, res) => {
+  res.send("meow");
 });
 
 module.exports = routes;
