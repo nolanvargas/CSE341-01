3681b38231fc27c62c162881dddba3095ba31439
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2023-01-07 17:41:33 -0700

    fixes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 
-const myController = require('../controllers');
-
-routes.get('/url', myController.getNames);
+routes.get('/', (req, res) => {
+  res.send('Sarah Birch');
+});
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var app = express();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use("/", require("./routes"));
+app.use('/', require('./routes'))
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
