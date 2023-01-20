b92f7877b27b6c749b40ab76a35213dac3ea1e61
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2023-01-19 18:32:29 -0700

    added test routes for post, put, delete

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,7 @@
 const mongodb = require("../db/connect");
 const ObjectId = require("mongodb").ObjectId;
 
-const getAllContacts = async (req, res, next) => {
+const getAllContacts = async (req, res) => {
   // Chained statement
   // mongodb.getDb -> database client
   // .db() -> here is where i would put the name of the database
@@ -25,7 +25,7 @@ const getAllContacts = async (req, res, next) => {
 //   });
 // };
 
-const getSingleContact = async (req, res, next) => {
+const getSingleContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
@@ -38,4 +38,65 @@ const getSingleContact = async (req, res, next) => {
   });
 };
 
-module.exports = { getAllContacts, getSingleContact };
+const postContact = async (req, res) => {
+  console.log(req.body);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    color: req.body.color,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error);
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  let updatedContact = {};
+  Object.entries(req.body).forEach(([key, value]) => {
+    updatedContact[key] = value;
+  });
+
+  console.log(updatedContact);
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .updateOne({ _id: userId }, { $set: updatedContact });
+
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error);
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .remove({ _id: userId }, true);
+  if (response.deletedCount > 0) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error);
+  }
+};
+module.exports = {
+  getAllContacts,
+  getSingleContact,
+  postContact,
+  updateContact,
+  deleteContact,
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -6,4 +6,7 @@ const router = express.Router();
 
 router.get("/", contactsController.getAllContacts);
 router.get("/:id", contactsController.getSingleContact);
+router.post("/", contactsController.postContact);
+router.put("/:id", contactsController.updateContact);
+router.delete("/:id", contactsController.deleteContact);
 module.exports = router;
