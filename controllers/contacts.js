49d8646ce067a16f958cc0eb5f1107ec5611d358
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2023-01-26 09:29:29 -0700

    added try catch blocks to each function

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,18 +3,17 @@ const mongodb = require("../db/connect");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAllContacts = async (req, res) => {
-  // Chained statement
-  // mongodb.getDb -> database client
-  // .db() -> here is where i would put the name of the database
-  // .collection('user') -> ?
-  // .find() -> ?
-  const result = await mongodb.getDb().db().collection("contacts").find();
+  try {
+    const result = await mongodb.getDb().db().collection("contacts").find();
 
-  // Convert the result to an array, then set the response header and status
-  result.toArray().then((lists) => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(lists);
-  });
+    // Convert the result to an array, then set the response header and status
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 // const getSingleContact = async (req, res, next) => {
@@ -26,73 +25,90 @@ const getAllContacts = async (req, res) => {
 // };
 
 const getSingleContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection("contacts")
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("contacts")
+      .find({ _id: userId });
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists[0]);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const postContact = async (req, res) => {
-  console.log(req.body);
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    color: req.body.color,
-    birthday: req.body.birthday,
-  };
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection("contacts")
-    .insertOne(contact);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error);
+  try {
+    console.log(req.body);
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      color: req.body.color,
+      birthday: req.body.birthday,
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("contacts")
+      .insertOne(contact);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error);
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
 const updateContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  let updatedContact = {};
-  Object.entries(req.body).forEach(([key, value]) => {
-    updatedContact[key] = value;
-  });
+  try {
+    const userId = new ObjectId(req.params.id);
+    let updatedContact = {};
+    Object.entries(req.body).forEach(([key, value]) => {
+      updatedContact[key] = value;
+    });
 
-  console.log(updatedContact);
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection("contacts")
-    .updateOne({ _id: userId }, { $set: updatedContact });
+    console.log(updatedContact);
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("contacts")
+      .updateOne({ _id: userId }, { $set: updatedContact });
 
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error);
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
 const deleteContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection("contacts")
-    .remove({ _id: userId }, true);
-  if (response.deletedCount > 0) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error);
+  try {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("contacts")
+      .remove({ _id: userId }, true);
+    if (response.deletedCount > 0) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error);
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
+
 module.exports = {
   getAllContacts,
   getSingleContact,
